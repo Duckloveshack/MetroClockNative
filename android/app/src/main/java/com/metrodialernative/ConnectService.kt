4a87c183685b53cf434e7b198c15da87eb329792
Duckloveshack <danieldandalii@gmail.com>
2025-03-05 09:38:46 -0800

    random stuff, my macbooks kinda weak :/

--- android/app/src/main/java/com/metrodialernative/ConnectService.kt
+++
@@ -1,8 +0,0 @@
-package io.github.duckloveshack.metro_dialer_native
-
-import android.telecom.ConnectionService
-import android.telecom.Call
-
-class ConnectionService(): ConnectionService() {
-    
-}
---
+++ android/app/src/main/java/io/github/duckloveshack/metro_dialer_native/CallService.kt
@@ -0,0 +1,7 @@
+package io.github.duckloveshack.metro_dialer_native
+
+import android.telecom.InCallService
+import android.telecom.Call
+
+class CallService(): InCallService() {
+}
